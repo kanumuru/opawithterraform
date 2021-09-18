62398899816ab2047295d922a7b98435806f31f1
Rajesh Reddy <kanumururajeshreddy@gmail.com>
2021-09-18 14:07:50 +0530

    Update resources.tf
    Finally working

--- resources.tf
+++ resources.tf
@@ -1,63 +1,14 @@
-# resource "random_string" "random_name" {
-#   length  = 10
-#   special = false
-#   upper   = false
-# }
-
-
-# resource "aws_s3_bucket" "opa_bucket" {
-#   bucket        = format("opa-encryption-%s", random_string.random_name.result)
-#   acl           = "private"
-#   force_destroy = true
-# }
-
-
-
-# resource "aws_subnet" "public-subnet" {
-#   cidr_block        = var.public_subnet_cidr
-#   vpc_id            = aws_vpc.stack-example-vpc.id
-#   availability_zone = "us-west-2a"
-
-#   tags = {
-#     department = "Learning"
-#   }
-# }
-
-
-
-# resource "aws_internet_gateway" "gateway" {
-#   vpc_id = aws_vpc.stack-example-vpc.id
-
-#   tags = {
-#     department = "Learning"
-#     owner      = "ASE"
-#   }
-
-# }
-
-
-# resource "aws_vpc" "stack-example-vpc" {
-#   cidr_block           = var.vpc_cidr
-#   enable_dns_support   = true
-#   enable_dns_hostnames = true
-#   tags = {
-#     owner = "ASE"
-#   }
-# }
-
-
-## IaC according to OPA policy
-
+resource "random_string" "random_name" {
+  length  = 10
+  special = false
+  upper   = false
+}
 
 
 resource "aws_s3_bucket" "opa_bucket" {
-  bucket        = "opa-encryption-testing-now-in-cicd"
+  bucket        = format("opa-encryption-%s", random_string.random_name.result)
   acl           = "private"
   force_destroy = true
-    tags = {
-    department = "Learning"
-    owner      = "ASE"
-  }
 }
 
 
@@ -69,7 +20,6 @@ resource "aws_subnet" "public-subnet" {
 
   tags = {
     department = "Learning"
-    owner      = "ASE"
   }
 }
 
@@ -91,7 +41,57 @@ resource "aws_vpc" "stack-example-vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    department = "Learning"
-    owner      = "ASE"
+    owner = "ASE"
   }
 }
+
+
+## IaC according to OPA policy
+
+
+
+# resource "aws_s3_bucket" "opa_bucket" {
+#   bucket        = "opa-encryption-testing-now-in-cicd"
+#   acl           = "private"
+#   force_destroy = true
+#     tags = {
+#     department = "Learning"
+#     owner      = "ASE"
+#   }
+# }
+
+
+
+# resource "aws_subnet" "public-subnet" {
+#   cidr_block        = var.public_subnet_cidr
+#   vpc_id            = aws_vpc.stack-example-vpc.id
+#   availability_zone = "us-west-2a"
+
+#   tags = {
+#     department = "Learning"
+#     owner      = "ASE"
+#   }
+# }
+
+
+
+# resource "aws_internet_gateway" "gateway" {
+#   vpc_id = aws_vpc.stack-example-vpc.id
+
+#   tags = {
+#     department = "Learning"
+#     owner      = "ASE"
+#   }
+
+# }
+
+
+# resource "aws_vpc" "stack-example-vpc" {
+#   cidr_block           = var.vpc_cidr
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
+#   tags = {
+#     department = "Learning"
+#     owner      = "ASE"
+#   }
+# }
